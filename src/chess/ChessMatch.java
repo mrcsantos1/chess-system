da73553ff14c4a5c51dfd3eb309b30d133548b16
Marcos <mrcsantos1@outlook.com>
2021-09-27 08:53:05 -0300

    .

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,12 +1,17 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
     private final Board board;
 
     public ChessMatch() {
         this.board = new Board(8, 8);
+
+        this.initialSetup();
     }
 
     public Board getBoard() {
@@ -22,5 +27,11 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    private void initialSetup() {
+        this.board.placePiece(new Rook(this.board, Color.WHITE), new Position(2, 1));
+        this.board.placePiece(new King(this.board, Color.BLACK), new Position(0, 4));
+        this.board.placePiece(new King(this.board, Color.WHITE), new Position(7, 4));
     }
 }
