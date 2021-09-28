008809ad99b4f535fadc0c10cc42fbcc8a40888d
Marcos <mrcsantos1@outlook.com>
2021-09-28 07:51:11 -0300

    Bishop

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -3,6 +3,7 @@ package chess;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
@@ -200,8 +201,10 @@ public class ChessMatch {
 
     private void initialSetup() {
         this.placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        this.placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         this.placeNewPiece('e', 1, new King(board, Color.WHITE));
-       this.placeNewPiece('h', 1, new Rook(board, Color.WHITE));
+        this.placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+        this.placeNewPiece('h', 1, new Rook(board, Color.WHITE));
         this.placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
         this.placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
         this.placeNewPiece('c', 2, new Pawn(board, Color.WHITE));
@@ -212,7 +215,9 @@ public class ChessMatch {
         this.placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
         this.placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        this.placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         this.placeNewPiece('e', 8, new King(board, Color.BLACK));
+        this.placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         this.placeNewPiece('h', 8, new Rook(board, Color.BLACK));
         this.placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
         this.placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
